2354604e8f95e9177d41b8ef46e13a31b3422977
BrunoQualhato <brunoqualhato1@gmail.com>
2019-05-04 10:57:54 -0300

    telas do projeto

---
+++ aplicacao/view/menu_lateral.php
@@ -0,0 +1,128 @@
+<nav class="menu">
+    <ul class="sidebar-menu metismenu" id="sidebar-menu">
+        <li class="active">
+            <a href="index.php">
+                <i class="fa fa-home"></i> Painel </a>
+        </li>
+        <li>
+            <a href="">
+                <i class="fa fa-th-large"></i> Cadastros <i class="fa arrow"></i>
+            </a>
+            <ul class="sidebar-nav">
+                <li>
+                    <a href="cadastro_coordenador.php"> Coordenador </a>
+                </li>  
+                <li>
+                    <a href="cadastro_funcionario.php"> Funcionário </a>
+                </li>   
+                <li>
+                    <a href="cadastro_setor.php"> Setor </a>
+                </li>
+                <li>
+                    <a href="cadastro_usuario.php"> Usuário </a>
+                </li>
+                
+            </ul>
+        </li>
+        <li>
+            <a href="">
+                <i class="fa fa-area-chart"></i> Consultas <i class="fa arrow"></i>
+            </a>
+            <ul class="sidebar-nav">
+                <li>
+                    <a href="charts-flot.html"> ######## </a>
+                </li>
+                <li>
+                    <a href="charts-morris.html"> ######## </a>
+                </li>
+                <li>
+                    <a href="charts-flot.html"> ######## </a>
+                </li>
+                <li>
+                    <a href="charts-morris.html"> ######## </a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="">
+                <i class="fa fa-table"></i> Relatórios <i class="fa arrow"></i>
+            </a>
+            <ul class="sidebar-nav">
+                <li>
+                    <a href="charts-flot.html"> ######## </a>
+                </li>
+                <li>
+                    <a href="charts-morris.html"> ######## </a>
+                </li>
+                <li>
+                    <a href="charts-flot.html"> ######## </a>
+                </li>
+                <li>
+                    <a href="charts-morris.html"> ######## </a>
+                </li>
+            </ul>
+        </li>
+        
+        <li>
+            <a href="">
+                <i class="fa fa-desktop"></i> Empréstimos <i class="fa arrow"></i>
+            </a>
+            <ul class="sidebar-nav">
+            <li>
+                    <a href="charts-flot.html"> ######## </a>
+                </li>
+                <li>
+                    <a href="charts-morris.html"> ######## </a>
+                </li>
+                <li>
+                    <a href="charts-flot.html"> ######## </a>
+                </li>
+                <li>
+                    <a href="charts-morris.html"> ######## </a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="">
+                <i class="fa fa-file-text-o"></i> Devoluções <i class="fa arrow"></i>
+            </a>
+            <ul class="sidebar-nav">
+            <li>
+                    <a href="charts-flot.html"> ######## </a>
+                </li>
+                <li>
+                    <a href="charts-morris.html"> ######## </a>
+                </li>
+                <li>
+                    <a href="charts-flot.html"> ######## </a>
+                </li>
+                <li>
+                    <a href="charts-morris.html"> ######## </a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="">
+                <i class="fa fa-file-text-o"></i> Empréstimos Atrasados <i class="fa arrow"></i>
+            </a>
+            <ul class="sidebar-nav">
+            <li>
+                    <a href="charts-flot.html"> ######## </a>
+                </li>
+                <li>
+                    <a href="charts-morris.html"> ######## </a>
+                </li>
+                <li>
+                    <a href="charts-flot.html"> ######## </a>
+                </li>
+                <li>
+                    <a href="charts-morris.html"> ######## </a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="#">
+            <i class="fa fa-power-off icon"></i> Sair </a>
+        </li>
+    </ul>
+</nav>
