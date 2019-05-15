7cc54c56b91077f65a98ca5e3af35c0a0706e139
brunoqualhato <brunoqualhato1@gmail.com>
2019-05-14 21:49:23 -0300

    adicionado verificador de sessao.

--- aplicacao/controller/DB.php
+++ aplicacao/controller/DB.php
@@ -2,20 +2,30 @@
 class DB {
 	private static $instance;
 	static function getInstance(){
-	if(!isset(self::$instance))	{
-		try{
-			self::$instance = new PDO("mysql:host=localhost;dbname=sistema_inventario", 'root', "123456789");
-			self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			self::$instance->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-		} catch (PDOException $e){
-			echo $e->getMessage();
+		if(!isset(self::$instance))	{
+			try{
+				self::$instance = new PDO("mysql:host=localhost;dbname=sistema_inventario", 'root', "123456789");
+				self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				self::$instance->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+				verificaSessao();
+			} catch (PDOException $e){
+				echo $e->getMessage();
+			}
 		}
-	}
 		return self::$instance;
 	}
-	 public static function prepare($sql) {
-        return self::getinstance()->prepare($sql);
-    }
+	public static function prepare($sql) {
+		return self::getinstance()->prepare($sql);
+	}
+
+	static function verificaSessao(){
+		if ((!isset($_SESSION['cpf']) && !isset($_SESSION['senha']))) {
+
+			header('Location: ../index.php');
+
+		}
+
+	}
 }
 
 ?>
