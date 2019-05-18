24eb2a7136ef281360a381de0b2ee18a1b11c801
BrunoQualhato <brunoqualhato1@gmail.com>
2019-05-18 12:44:51 -0300

    adicionado a interação para cadastrar o coordenador

--- aplicacao/controller/CoordenadorController.php
+++ aplicacao/controller/CoordenadorController.php
@@ -1,7 +1,9 @@
 <?php
 require 'DB.php';// Classe responsavel pela conexão ao banco de dados
 require '../model/Coordenador.php';// classe responsavel por fazer manipular o login do usuario
+require '../model/Cidade.php'; //classe responsavel por manipular todo registro das cidades
 
+// if o login
 if(isset($_POST['entrar'])){ //verifica se o botao de logar foi submetido
 	$formulario_login =  array(
         'cpf' => $_POST['cpf'], //adiciono no array o cpf
@@ -21,4 +23,36 @@ if(isset($_POST['entrar'])){ //verifica se o botao de logar foi submetido
 		//exit();
 	}
 }
+
+//if para cadastro de coordenador
+if (isset($_POST['salvar'])) {
+    $dadosFormularioCoordenador = array(
+        'nome' =>$_POST['nome'],
+        'CPF' => $_POST['cpf'],
+        'telefone' => $_POST['telefone'],
+        'email' => $_POST['email'],
+        'sexo' => $_POST['sexo'],
+        'endereco' => $_POST['endereco'],
+        'id_cidade' => $_POST['cidade'],
+        'senha' => md5($_POST['senha'])
+    );
+    $oCoordenador = new Coordenador(DB::getInstance(), $dadosFormularioCoordenador);
+    if($oCoordenador->inserir()){
+        header("Location: ../view/cadastro_coordenador.php?salvo=true");
+    }else{
+        //eita
+    }
+}
+
+function listaCidades(){
+    $oCidade = new Cidade(DB::getInstance(), null);
+    return $oCidade->listar();
+}
+
+function listaCoordenadres() {
+    $oCoordenador = new Coordenador(DB::getInstance(), null);
+    return $oCoordenador->listar();
+}
+
+
 ?>
