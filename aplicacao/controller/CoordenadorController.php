c6e38d24147dcf950167a7e9c8bea654098e57d1
BrunoQualhato <brunoqualhato1@gmail.com>
2019-05-18 10:01:05 -0300

    correção do login

--- aplicacao/controller/CoordenadorController.php
+++ aplicacao/controller/CoordenadorController.php
@@ -1,7 +1,7 @@
 <?php
 require 'DB.php';// Classe responsavel pela conexão ao banco de dados
 require '../model/Coordenador.php';// classe responsavel por fazer manipular o login do usuario
-session_start();
+
 if(isset($_POST['entrar'])){ //verifica se o botao de logar foi submetido
 	$formulario_login =  array(
         'cpf' => $_POST['cpf'], //adiciono no array o cpf
@@ -10,16 +10,15 @@ if(isset($_POST['entrar'])){ //verifica se o botao de logar foi submetido
 	$banco = DB::getInstance();
 
 	$oLoginService = new Coordenador($banco,$formulario_login);
-	$dados = $oLoginService->login();
-	if (count($dados)){ //corrigir isso depois
-		$_SESSION['dados_usuario'] = $dados;
-		header('Location: ../view/index.php');
-	//	exit();
-	} else{
-		header('Location: ../index.php');
-		exit();
+	$dados = $oLoginService->login();// chama o metodo do login
+	if ($dados != null){ //verifica se não está nulo
+	    session_start();//inicia a sessão
+		$_SESSION['dados_usuario'] = $dados; //joga os dados do usuario na sessão
+		header('Location: ../view/index.php');//redireciona para a pagina principal
+	   //exit();
+	} else{ //se for nulo
+		header('Location: ../index.php'); // redireciona para ele mesmo
+		//exit();
 	}
-
-
 }
 ?>
