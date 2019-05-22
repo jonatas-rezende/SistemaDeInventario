1945d84abc5069ac79008e788efcf35661296512
jonatas-rezende <jonatassrezende@gmail.com>
2019-05-21 22:26:39 -0300

    atualizacoes

--- aplicacao/model/Emprestimo.php
+++ aplicacao/model/Emprestimo.php
@@ -4,6 +4,7 @@
 
 		private $conexao;
 		private $emprestimo;
+        private $itens_emprestimo;
 
 		private $idFuncionario;
 		private $data;
@@ -18,25 +19,35 @@
 		    $this->$atributo = $valor;
 		}		
 
-		public function __construct(Conexao $conexao, $emprestimo) {
-            $this->conexao = $conexao->conectar();
-            $this->coordenador = $emprestimo;
+		public function __construct($conexao, $emprestimo, $itens_emprestimos) {
+            $this->conexao = $conexao;
+            $this->emprestimo = $emprestimo;
+            $this->itens_emprestimo = $itens_emprestimo;
+
         }
 
         public function inserir() {
 
             try {
 
-                $sql = '';
+                $sql = 'INSERT INTO emprestimos (id_funcionario, data, data_devolucao, observacao) VALUES (?, ?, ?, ?)';
                 $stmt = $this->conexao->prepare($sql);
-                $stmt->bindValue(1, $this->funcionario['nome']);
+                $stmt->bindValue(1, $this->emprestimo['id_funcionario']);
+                $stmt->bindValue(2, $this->emprestimo['data']);
+                $stmt->bindValue(3, $this->emprestimo['data_devolucao']);
+                $stmt->bindValue(4, $this->emprestimo['observacao']);
                 $stmt->execute();
 
-                $sql1 = '';    
 
-                $stmt1 = $this->conexao->prepare($sql1);
-                $stmt1->bindValue(1, $this->conexao->lastInsertId());
-                return $stmt1->execute();
+                $lastId = $this->conexao->lastInsertId() //pegar o ultimo ID de cadastro emprestimos
+
+                foreach ($this->itens_emprestimos as $item) {
+                    $sql1 = 'INSERT INTO itens_emprestimos (id_emprestimo, id_item) VALUES (?, ?)';    
+                    $stmt1 = $this->conexao->prepare($sql1);
+                    $stmt1->bindValue(1, $lastId);
+                    $stmt1->bindValue(2, $item['id_item']);
+                    $stmt1->execute();
+                }
 
             } catch (PDOException $e) {
                 echo $e->getMessage();
@@ -50,7 +61,7 @@
                 $sql = '';
                 $stmt = $this->conexao->prepare($sql);
                 $stmt->execute();
-                return $stmt->fetchAll(PDO::FETCH_ASSOC);
+                return $stmt->fetchAll(PDO::FETCH_OBJ);
 
             } catch (PDOException $e) {
                 echo $e->getMessage();
